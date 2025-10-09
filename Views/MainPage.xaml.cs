2ab2cd8f818123efe2e9ee589e6bb976a8afc39a
Marmag8 <bartek.warzecha02@gmail.com>
2025-10-09 10:42:32 +0200

    Full functionality for dynamically adding new counters (scroll view to be fixed)

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,31 +1,82 @@
 ﻿using Licznik.Backend;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Licznik.Views
 {
     public partial class MainPage : ContentPage
     {
-        List<Counter> counters = new List<Counter>();
-        
+        readonly List<Counter> counters = new List<Counter>();
+        int counterIndex = 1;
 
         public MainPage()
         {
             InitializeComponent();
-            counters.Add(new Counter(0, "counter1"));
+            AddCounter("counter1", 0);
         }
 
-        private void OnPlusBtnClicked(object? sender, EventArgs e)
+        private async void OnAddCounterClicked(object sender, EventArgs e)
         {
-            Counter counter = counters.Find(x => x.name == "counter1");
-            counter.Increment();
-            CounterLabel.Text = counter.count.ToString();
+            counterIndex++;
+            string name = $"counter{counterIndex}";
+
+            string result = await DisplayPromptAsync(
+                "Nowy licznik",
+                $"Podaj wartość początkową dla licznika {name}:",
+                "OK",
+                "Anuluj",
+                "0",
+                keyboard: Keyboard.Numeric);
+
+            if (int.TryParse(result, out int startValue)) AddCounter(name, startValue);
+            else AddCounter(name, 0);
         }
 
-        private void OnMinusBtnClicked(object? sender, EventArgs e)
+        private void AddCounter(string name, int startValue)
         {
-            Counter counter = counters.Find(x => x.name == "counter1");
-            counter.Decrement();
-            CounterLabel.Text = counter.count.ToString();
+            var counter = new Counter(startValue, name);
+            counters.Add(counter);
+
+            var label = new Label
+            {
+                Text = $"{name}: {counter.count}",
+                FontSize = 18,
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = new Thickness(0, 20, 0, 0)
+            };
+
+            var plusBtn = new Button
+            {
+                Text = "+",
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = new Thickness(5, 5, 5, 5)
+            };
+
+            plusBtn.Clicked += (s, e) =>
+            {
+                counter.Increment();
+                label.Text = $"{name}: {counter.count}";
+            };
+
+            var minusBtn = new Button
+            {
+                Text = "-",
+                HorizontalOptions = LayoutOptions.Center,
+                Margin = new Thickness(5, 5, 5, 5)
+            };
+            minusBtn.Clicked += (s, e) =>
+            {
+                counter.Decrement();
+                label.Text = $"{name}: {counter.count}";
+            };
+
+            HorizontalStackLayout btns = new HorizontalStackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                Children = { plusBtn, minusBtn }
+            };
+
+            CountersLayout.Children.Add(label);
+            CountersLayout.Children.Add(btns);
         }
     }
 }
