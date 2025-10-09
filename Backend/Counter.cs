b0969d8139ff20bfbf62c2609b2081e1e8dc9aa3
Marmag8 <bartek.warzecha02@gmail.com>
2025-10-09 09:15:54 +0200

    Refactor, basic functionality for dynamically adding new counters

---
+++ Backend/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licznik.Backend
+{
+    internal class Counter
+    {
+        private int _count;
+        public int count 
+        { 
+            get { return _count; } 
+            set { _count = value; }
+        }
+
+        private String _name;
+        public String name 
+        { 
+            get { return _name; } 
+            set { _name = value; }
+        }
+
+        public Counter(int count, String name)
+        {
+            this.count = count;
+            this.name = name;
+        }
+
+        public void Increment()
+        {
+            count++;
+        }
+
+        public void Decrement()
+        {
+            count--;
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Licznik.Backend;
+using System.Collections;
+
+namespace Licznik.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Counter> counters = new List<Counter>();
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            counters.Add(new Counter(0, "counter1"));
+        }
+
+        private void OnPlusBtnClicked(object? sender, EventArgs e)
+        {
+            Counter counter = counters.Find(x => x.name == "counter1");
+            counter.Increment();
+            CounterLabel.Text = counter.count.ToString();
+        }
+
+        private void OnMinusBtnClicked(object? sender, EventArgs e)
+        {
+            Counter counter = counters.Find(x => x.name == "counter1");
+            counter.Decrement();
+            CounterLabel.Text = counter.count.ToString();
+        }
+    }
+}
